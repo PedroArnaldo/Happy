a3e01cc16c568ac9c4329c33b89e9276a7c6acbe
PedroArnaldo <pedromadeira201502@outlook.com>
2020-10-18 17:57:45 -0300

    first commit

---
+++ scripts/page-orphanages.js
@@ -0,0 +1,31 @@
+//create map
+const map = L.map('mapid').setView([-22.6781193,-43.2473869], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+//create icon
+const icon = L.icon({
+
+    iconUrl: "../Img/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+//create popup overlay
+const popup = L.popup({
+
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240, 
+    minHeight: 240
+
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage" <img src="../Img/arrow-white.svg"> </a>')
+
+//create and add marker 
+L.marker([-22.6781193,-43.2473869], { icon }).addTo(map)
+    .bindPopup(popup)
+    
